a6e9292a506cc4714c6faa7af4e72d9d9c7aa985
heymrspaceman <tam.mackenzie@gmail.com>
2013-02-10 15:57:58 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scimoc::Application.config.secret_token = 'e50069458df1fd1b5b031129ce2a759907cfa061e43ecb5f391705bbd7f77878c32599acdca91cd0d4561b9ed90fdb926a04c79627557b0587a6cada8292043a'
